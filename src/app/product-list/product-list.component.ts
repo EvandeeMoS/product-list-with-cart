d812678394110588a9daf29ccbe79b8cf8950509
EvandeeMoS <moisesevander@gmail.com>
2024-08-25 16:12:11 -0300

    feat: adjust text color of product-list title, remove border from main component

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Inject } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NgFor, CurrencyPipe } from '@angular/common';
 import { ProductService } from '../product.service';
 import { Product } from './product.type';
